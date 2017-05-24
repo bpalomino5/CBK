c106fc9ccca090acc6d4766856d087ee278a237f
Brandon Palomino <12bpalomino@gmail.com>
2017-05-23 19:49:29 -0700

    added parser

--- Kilian_Christopher Palomino_Brandon Purohit_Kunal.pl
+++ Kilian_Christopher Palomino_Brandon Purohit_Kunal.pl
@@ -40,3 +40,28 @@ pro(subject) --> [capacity].
 pro(object) --> [blue].
 pro(object) --> [liter].
 prep --> [of, the].
+
+/*
+Added Parser that parses input and returns tree to first argument
+ex.
+?- s(T,[the,color,of,the,car,is,blue],[]).
+T = s(np(det(the), pro(color)), pp(prep(of, the), n(car)), vp(v(is), np(pro(blue)))) .
+*/
+
+s(s(NP,PP,VP)) --> np(subject,NP), pp(PP), vp(VP).
+np(_,np(Det,N)) --> det(Det), n(N).             
+np(X,np(Det,Pro)) --> det(Det), pro(X,Pro).
+np(X,np(Pro)) --> pro(X,Pro).
+pp(pp(PREP,N)) --> prep(PREP), n(N).
+vp(vp(V,NP)) --> v(V), np(object,NP).
+vp(vp(V)) --> v(V).
+det(det(the)) --> [the]. 
+det(det(one)) --> [one].              
+n(n(car)) --> [car].       
+n(n(bottle)) --> [bottle].
+v(v(is)) --> [is].
+pro(subject,pro(color)) --> [color].
+pro(subject,pro(capacity)) --> [capacity].
+pro(object,pro(blue)) --> [blue].
+pro(object,pro(liter)) --> [liter].
+prep(prep(of, the)) --> [of, the].
