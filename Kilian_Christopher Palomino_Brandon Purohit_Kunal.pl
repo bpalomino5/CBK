09ec8772bb412b27d91de984a787b46625755bfd
Christopher Kilian <cdkilian@gmail.com>
2017-06-02 16:53:15 -0700

    Major update to all comments, cleanup of code structure, removal of extraneous DCG and lexicon elements, addition of second question sentence type,

--- Kilian_Christopher Palomino_Brandon Purohit_Kunal.pl
+++ Kilian_Christopher Palomino_Brandon Purohit_Kunal.pl
@@ -1,193 +1,159 @@
-% Christopher Kilian
-% Brandon Palomino
-% Kunal Purohit
-
-% CS 352 - Spring 2017
-% Prolog Project
-
 /*
-Following grammer handles:
-the color of the car is blue
-the capacity of the bottle is one liter
+Christopher Kilian
+Brandon Palomino
+Kunal Purohit
 
-Console:
-s([the,color,of,the,car,is,blue],[]).
-true.
+CS 352 - Spring 2017
+Prolog Project
 
-s([the,capacity,of,the,bottle,is,one,liter],[]).
-true.
+This prolog program is designed to parse simple statements of fact about the properties of objects, and simple questions of those facts.
+It can handle statements in the form: "the <attribute> of the <object> is <value>" (for example, "the length of the rod is 10 inches")
+It can handle questions in two forms, either: "what is the <attribute> of the <object>" (for example, "what is the capacity of the bottle")
+or the form: "what <attribute> is the <object>" (for example, "what color is the car")
 
-Implementation:
-pp: prepositional phrase in between noun phrase and verb phrase
-prep: preposition statement
-i.e. [of,the]
-since thats the only prepositonal phrase in our example
-
-Additional implementation (for questions):
-noun phrase (interrogative), verb phrase
-
-Adding pronoun "what" - classified as an interrogative, along with "who", "why", "where", "when" (not adding these yet)
-useful classifications can be seen here: https://www.ucl.ac.uk/internet-grammar/nouns/pronoun.htm 
-And phrase structures can be seen here: https://faculty.washington.edu/wassink/LING200/lect14_syntax2.pdf 
-Other useful definitions:
-A prepositional phrase is a group of words containing a preposition, a noun or pronoun object of the preposition, and any modifiers of the object.
-
-English sentence structure rules (note values in parenthesis are optional):
-s --> np, vp
-np --> (det) (adj) n (pp)
-vp --> v (np) (pp) (adv)
-pp --> p (np)
-
-Ex: The color of the car is blue.
-s(np(det(the), n(color), pp(p(of), np(det(the), n(car)))), vp(v(is), np(n(blue))))
-
-Todo: How to handle a sentence like "what color is the car?" --> doesn't follow standard rules I've seen so far
-since it allows a noun directly following a pronoun. In our language, perhaps allow for a verb phrase that starts with a noun phrase?
+The words used for <attribute>, <object>, and <value> are open ended since this program uses an open lexicon with regards to nouns and adjectives, allowing any values for these
+three attributes.
 */
 
-
-:- dynamic fact/3. % declaration that there is a dynamic predicate named "fact" with an arity of 3
-
+% Allow for the dynamic creation of "fact"'s in the database at runtime - "fact" is designed to hold statement information in the form: fact(Attribute, Object, Value)
+:- dynamic fact/3. 
 
 
 /*
-Edited parser to match updated DCG
-ex 1: ?- s(T, [the, color, of, the, car, is, blue], []).
-T = s(np(det(the), n(color), pp(prep(of), np(det(the), n(car)))), vp(v(is), np(n(blue)))) 
-
-ex 2: ?- s(T, [what, is, the, color, of, the, car], []).
-T = s(np(pro(what)), vp(v(is), np(det(the), n(color), pp(prep(of), np(det(the), n(car))))))
+DCG\Parse Tree found here:
+This DCG will create a parsed tree of the sentences provided to the "execute" predicate at runtime.
+It is designed to handle sentences of the types as described in the comment at the top of this program.
+Note that "values" can be either single or double word in nature - for example, the value can be something like "blue", "green", "smooth", etc or it can have the form "20 inches", "dark orange", etc.
+The DCG uses a lexicon (found below the DCG) which allows for an open lexicon with regards to either nouns or adjectives, allowing sentences of the above formats to 
+be accepted whether the noun\adjective in question has been seen before or not. 
 */
-
-% DCG/Parse Tree found here:
-
 s(s(NP, VP)) --> np(subject,NP); np(interrogative,NP), vp(VP).
-np(_,np(N)) --> n(N).      
-np(_,np(Det, N)) --> det(Det), n(N).
-np(_,np(Adj, N)) --> adj(Adj), n(N).
-np(_,np(Det, Adj, N)) --> det(Det), adj(Adj), n(N).
-np(_,np(Adj, N, PP)) --> adj(Adj), n(N), pp(PP).
-np(_,np(Det, Adj, N, PP)) --> det(Det), adj(Adj), n(N), pp(PP).
-np(_,np(Det, N, PP)) --> det(Det), n(N), pp(PP).           
-np(X,np(Det, Pro)) --> det(Det), pro(X, Pro).
-np(X,np(Pro)) --> pro(X,Pro).       
-pp(pp(PREP, NP)) --> prep(PREP), np(subject, NP). %note that "subject" here is placeholder --> need to figure out proper np type for prep phrases
+np(_, np(N)) --> n(N).      
+np(_, np(Det, N)) --> det(Det), n(N).
+np(_, np(Adj, N)) --> adj(Adj), n(N).
+np(_, np(Det, Adj, N)) --> det(Det), adj(Adj), n(N).
+np(_, np(Adj, N, PP)) --> adj(Adj), n(N), pp(PP).
+np(_, np(Det, Adj, N, PP)) --> det(Det), adj(Adj), n(N), pp(PP).
+np(_, np(Det, N, PP)) --> det(Det), n(N), pp(PP).           
+np(X, np(Det, Pro)) --> det(Det), pro(X, Pro).
+np(X, np(Pro)) --> pro(X,Pro).       
+np(X, np(Pro, N)) --> pro(X, Pro), n(N).       
+pp(pp(Prep, NP)) --> prep(Prep), np(_, NP).
 vp(vp(V, NP)) --> v(V), np(object, NP).
 vp(vp(V)) --> v(V).
 det(det(Word)) --> [Word], {lex(Word, det)}.
 n(n(Word)) --> [Word], {lex(Word, n)}.
 v(v(Word)) --> [Word], {lex(Word, v)}.
 adj(adj(Word)) --> [Word], {lex(Word, adj)}.
-pro(subject, pro(he)) --> [he].
-pro(subject, pro(she)) --> [she].
-pro(object, pro(him)) --> [him].
-pro(object, pro(her)) --> [her].
 pro(interrogative, pro(Word)) --> [Word], {lex(Word, pro(interrogative))}.
 prep(prep(of)) --> [of].
 
 
-% Lexicon defitions found here:
-
+/*
+Lexicon definitions found here:
+Note that the lexicon is designed to allow for open use of nouns or adjectives.
+*/
 lex(the, X) :- !, X = det.
-lex(one, X) :- !, X = det.
 lex(a, X) :- !, X = det.
 lex(an, X) :- !, X = det.
 lex(is, X) :- !, X = v.
-lex(he, X) :- !, X = pro(subject).
-lex(she, X) :- !, X = pro(subject).
-lex(him, X) :- !, X = pro(object).
-lex(her, X) :- !, X = pro(object).
 lex(what, X) :- !, X = pro(interrogative).
 lex(_, n).
 lex(_, adj).
 
 
 /*
-First attempt at "execute" function
-Idea being that execute will first run the passed sentence through the parser, and then will send the parsed sentence
-to another function which will in turn read the parsed sentence and pull out the relevant data.
+The "execute" predicate has an arity of 2, and expects the user to enter a sentence as a list of words as the first argument, and a variable as the second argument (the value to be returned)
+execute will first run the passed sentence through the parser, and then will send the parsed sentence to another predicate which will in turn read the parsed sentence and pull out the relevant data.
 */
 execute([], R) :- !, R = "You submitted a blank sentence!".
 execute(List, Response) :- s(T, List, []), readParsed(T, Response), !.
 execute(_, R) :- R = "I can\'t understand this sentence".
 
+
 /*
-The first argument of the sentence is the noun phrase. If the first argument to the noun phrase is the pronoun "what", it's a question and should be handled accordingly.
-If the sentence is not a question, it can be handled as a statement instead.
+"readParsed" predicate has an arity of 2 and expects a fully parsed sentence tree as its first argument, along with an uninstantiated variable as the second (this is the response to be returned)
+This predicate will examine the arguments of the sentence tree to determine if it's a question or a statement. If it's a question, then the first argument to the noun phrase is the pronoun "what", 
+and if this is the case the sentence tree is passed to the "processQuestion" predicate for handling.
+If the sentence is not a question, it can be handled as a statement instead and is passed to the "processStatement" predicate instead.
 */
 readParsed(ParsedSentence, Response) :- arg(1, ParsedSentence, X), arg(1, X, pro(what)), !, processQuestion(ParsedSentence, Response).
 readParsed(ParsedSentence, Response) :- processStatement(ParsedSentence, Response).
 
+
 /*
-Handle the dissection of questions
+"processQuestion" predicate has an arity of 2, and expects the fully parsed sentence tree question as the first argument with an uninstantiated variable as its second (to be unified with the return value)
+This predicate makes extensive use of the "arg" predicate to break down the sentence. Since the sentence is a question, the expected structure is known (see comment for the DCG), and the arg predicates
+can be called in a sequential fashion to pull out the relevant "object" and "attribute" information. Those values, along with the uninstantiated response variable, are then passed to the "checkDBQuestion"
+predicate for database evaluation and response unification.
+The first rule handles questions of the form: "what is the <attribute> of the <object>"
+The second rule handles questions of the form: "what <attribute> is the <object>"
 */
 processQuestion(ParsedSentence, Response) :- arg(2, ParsedSentence, VP), arg(2, VP, NP), arg(2, NP, n(Attribute)), arg(3, NP, PP), arg(2, PP, NNP), 
-                                                                       arg(2, NNP, n(Object)), checkDBQuestion(Attribute,Object,Response).
+                                                                         arg(2, NNP, n(Object)), !, checkDBQuestion(Attribute,Object,Response).																   
+processQuestion(ParsedSentence, Response) :- arg(1, ParsedSentence, NP), arg(2, ParsedSentence, VP), arg(2, NP, n(Attribute)), arg(2, VP, NNP),
+                                                                         arg(2, NNP, n(Object)), !, checkDBQuestion(Attribute,Object,Response).
+																	   										   
 																	   
-
-checkDBQuestion(Attribute,Object,Response) :- fact(Attribute,Object,Value), !, string_concat("It\'s ", Value, Response).
-checkDBQuestion(_,_,Response) :- Response = "I don\'t know".   % printMessage(Attribute,Object,'unknown', Response). MODIFIED
-
-% printMessage predicate
-% Added to help clean up printing to user
-% Concatenates Attribute, Object, and Value to a single line string, and unifies that string to Response
-% Used in checkDBQuestion and checkDB
-printMessage(Attribute, Object, Value, Response) :- term_string(Attribute, Att), term_string(Object, Obj), string_concat("The ",Att, P1), string_concat(P1, " of the ", P2), string_concat(P2, Obj, P3), string_concat(P3, " is ", P4), string_concat(P4, Value, Response).
-
-%ASK PROFESSOR - Is this sentence return val OK, or does it need to exactly match the assignment outline?
-
 /*
-Handle the dissection of statements
-Sample sentence breakdown handled here:
-First example here looks at arg 1 of the parsed sentence which is np(det(the), n(color), pp(prep(of), np(det(the), n(car)))), unifying with "NP"
-also gets the second argument for the parse sentence, which is vp(v(is), np(n(blue))), unifying this with VP
+"processStatement" predicate has an arity of 2, and expects the fully parsed sentence tree statement as the first argument with an uninstantiated variable as its second (to be unified with the return value)
+This predicate makes extensive use of the "arg" predicate to break down the sentence (working similarly to the "processQuestion" predicate). Since the sentence is a statement, the expected structure is known 
+(see comment for the DCG), and the arg predicates can be called in a sequential fashion to pull out the relevant "attribute", "object", and "value" information from the sentence.
+Those values, along with the uninstantiated response variable, are then passed to the "checkDB" predicate for database evaluation and response unification.
+
+The following is a sample breakdown of a sentence "the color of the car is blue" to demonstrate the operation of these "process" predicates:
+looks at arg 1 of the parsed sentence which is np(det(the), n(color), pp(prep(of), np(det(the), n(car)))), unifying with "NP"
+next gets the second argument for the parse sentence, which is vp(v(is), np(n(blue))), unifying this with VP
 then it looks at arg 2 of NP which is n(color), and "color" is unified with "Attribute"
 then looks at arg 3 of the noun phrase which is pp(prep(of), np(det(the), n(car))), unifying with PP
 then looks at arg 2 of PP which is np(det(the), n(car)), unifying with NNP (new noun phrase)
 then looks at arg 2 of NNP which is n(car), unifying "car" with "Object"
 then looks at arg 2 of VP which is np(n(blue)) and unifies with NNNP (new new noun phrase)
-finally looking at arg 1 of NNNP which is n(blue), unifying blue with "Value"
+finally, NNNP is passed to the "getValue" predicate which will determine if it's a 1 or 2 word value, and unify the string representation of that value with "Value"
 */
 processStatement(ParsedSentence, Response) :- arg(1, ParsedSentence, NP), arg(2, ParsedSentence, VP),
-                                                                       arg(2, NP, n(Attribute)), arg(3, NP, PP), arg(2, PP, NNP), 
-                                                                       arg(2, NNP, n(Object)), arg(2, VP, NNNP), getValue(NNNP, Value), checkDB(Attribute, Object, Value, Response).
-
-% getValue predicate
-% Handles getting single or multi-word properties from the term NP, which is the term inside VP of the parsed tree.
-% Concatenates the property values together and unifies to term Value.
-% Used in processStatement
-getValue(NP, Value) :- functor(NP,_,A), A=:=1, !, arg(1, NP, n(N)), term_string(N, Value). %modified to change value to a string in advance
-getValue(NP, Value) :- functor(NP,_,A), A=:=2, arg(1, NP, det(D)), !, arg(2, NP, n(N)), term_string(D, V1), term_string(N, V2), string_concat(V1, " ", P1), string_concat(P1, V2, Value).
-getValue(NP, Value) :- functor(NP,_,A), A=:=2, arg(1, NP, adj(Adj)), !, arg(2, NP, n(N)), term_string(Adj, V1), term_string(N, V2), string_concat(V1, " ", P1), string_concat(P1, V2, Value).
-
+                                                                          arg(2, NP, n(Attribute)), arg(3, NP, PP), arg(2, PP, NNP), 
+                                                                          arg(2, NNP, n(Object)), arg(2, VP, NNNP), getValue(NNNP, Value), checkDB(Attribute, Object, Value, Response).											   
+																	   
+																	   
+/*
+"checkDB" predicate has an arity of 4, and expects "Attribute" and "Object" and "Value" to be instantiated. "Response" is always expected to be
+uninstantiated at first. This predicate is designed to handle the values passed in from a statement sentence. The first rule checks to see if the dynamic "fact" with these three values already exists 
+in the DB, and if it does, unifies "Response" to be "I know". If the combination of these three things in "fact" doesn't exist in the DB, the second rule checks to see if
+the "Attribute" and "Object" combination exists in the DB. If it does, the "Value" associated with those other two items is unified to "X", and all three values (along with "Response") are passed to the 
+"printMessage" predicate for response handling. Finally, if neither of these rules applies, then a new "fact" needs to be created in the DB, and this is handled by the "assert" predicate call.
+*/
 checkDB(Attribute, Object, Value, Response) :- fact(Attribute, Object, Value), !, Response = "I know".
 checkDB(Attribute, Object, _, Response) :- fact(Attribute, Object, X), !, printMessage(Attribute, Object, X, Response).
-checkDB(Attribute, Object, Value, Response) :- assert(fact(Attribute, Object, Value)), !, Response = "OK".
-
+checkDB(Attribute, Object, Value, Response) :- assert(fact(Attribute, Object, Value)), !, Response = "OK".	
 
 
 /*
-05\26\17 update:
-Hey guys, I talked to the professor after class yesterday and got a good explanation of how we should handle the checking\adding of info to our DB. We can't use a variable
-as the functor because Prolog won't allow that, but we CAN create a predefined functor to store all of our data in.
-In other words, we can't use a statement like: Attribute(Object, Value)
-but we can say: fact(Attribute, Object, Value)
-
-Another option is to use the "functor" function defined in the slides to build these items on the fly. The professor said that wasn't necessary for this project,
-but after we've got this thing working I might go ahead and try that simply because it might be more flexible and might earn us some extra points!
-
-I've currently added our first data entry/check. If you run:
-?- execute([the, color, of, the, car, is, blue], X).
-it will add the fact to the DB and return "OK."
-Can test by first running: fact(color, car, blue). which will return "false" at first. Then run the above execute statement, and then the fact statement a second time.
-The second time it will return "true".
+The "checkDBQuestion" predicate has an arity of 3 and operates somewhat similarly to the "checkDB" predicate, but this one is designed to handle questions rather than statements.
+In a question sentence, only "Attribute" and "Object" can be retrieved from the sentence, and these two values are expected to be instantiated by this predicate. "Response", as usual, is expected
+to be uninstantiated. The first rule checks to see if the "Attribute" and "Object" combination exists in the database. If it does, the value associated with that pairing is unified with "Value", and this 
+value is concatenated with "It's " and finally unified to "Response". If the fact doesn't exist in the DB, the second rule simply unifies the Response to "I don't know".
 */
+checkDBQuestion(Attribute,Object,Response) :- fact(Attribute,Object,Value), !, string_concat("It\'s ", Value, Response).
+checkDBQuestion(_,_,Response) :- Response = "I don\'t know".
 
 
+/*
+The "getValue" predicate handles getting single or multi-word values from the term NP which appears inside the VP of the parsed tree.
+It concatenates the property values together as a single string and unifies to term Value. In the case that it's a single word value, that value is still converted
+to a string before being unified to "Value" (this is done so that "Value" handling can always assume a string by the "check" and "printMessage" predicates).
+*/
+getValue(NP, Value) :- functor(NP,_,A), A=:=1, !, arg(1, NP, n(N)), term_string(N, Value).
+getValue(NP, Value) :- functor(NP,_,A), A=:=2, arg(1, NP, det(D)), !, arg(2, NP, n(N)), term_string(D, V1), term_string(N, V2), string_concat(V1, " ", P1), string_concat(P1, V2, Value).
+getValue(NP, Value) :- functor(NP,_,A), A=:=2, arg(1, NP, adj(Adj)), !, arg(2, NP, n(N)), term_string(Adj, V1), term_string(N, V2), string_concat(V1, " ", P1), string_concat(P1, V2, Value).
 
 
-
-
-
+/*
+The "printMessage" predicate has an arity of 4, and expects all passed values except "Response" to be instantiated. It's essentially a helper function, designed to be used when a statement has 
+been processed that found contradictory information already in the DB. For example, if fact(color, car, "blue") already exists in the DB and the statement "the color of the car is green" is processed, 
+this predicate is used to build the return response stating that the car is in fact "blue". Primarily used this way to clean up Response unification in the "checkDB" predicate.
+Concatenates Attribute, Object, and Value to a single line string, and unifies that string to Response.
+*/
+printMessage(Attribute, Object, Value, Response) :- term_string(Attribute, Att), term_string(Object, Obj), string_concat("No, the ",Att, P1), string_concat(P1, " of the ", P2), string_concat(P2, Obj, P3), string_concat(P3, " is ", P4), string_concat(P4, Value, Response).
 
 
